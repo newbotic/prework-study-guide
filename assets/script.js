ff2bb300f3512b0cfb201cf079e6d844eb401d56
newbotic <like_plus@yahoo.es>
2023-09-26 22:22:45 +0100

    update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,27 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (var x = 0; x < topics.length; x++) {
-  console.log(topics[x]);
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
 }
 
-/*var topic = "HTML";
+function selectTopic() {
+  if (topics[0] === "HTML") {
+    console.log("Let's study HTML!");
+  } else if (topics[0] === "CSS") {
+    console.log("Let's study CSS!");
+  } else if (topics[0] === "Git") {
+    console.log("Let's study Git!");
+  } else if (topics[0] === "JavaScript") {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log("Please try again!");
+  }
+}
 
-if (topic === "HTML") {
-  console.log("Let's study HTML!");
-} else if (topic === "CSS") {
-  console.log("Let's study CSS!");
-} else if (topic === "Git") {
-  console.log("Let's study Git!");
-} else if (topic === "JavaScript") {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log("Please try again!");
-} 
-*/
+console.log("Here are the topics we learned through Prework:");
+listTopics();
+console.log("Which topic should we study first?");
+selectTopic();
